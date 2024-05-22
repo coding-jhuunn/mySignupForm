72cd801cb40571e643e05bdc04e3d25e054692d7
coding-jhuunn <codingatjhunn@gmail.com>
2024-05-22 15:20:45 +0800

    done with the functions

--- script.js
+++ script.js
@@ -7,14 +7,41 @@ const inputEmail = document.getElementById("inputEmail");
 
 const labelError = document.getElementById("labelError");
 
+const popup = document.getElementById("popup");
+
+const main = document.getElementById("main");
+
+const popupDetail = document.getElementById("popupDetail");
+const popUpDiv = document.getElementById("divPopUp");
+const popUpBtn = document.getElementById("popupBtn");
+
 subscribeBtn.addEventListener("click", () => {
   if (validateEmail(inputEmail.value) === null) {
     console.log("error");
-    labelError.classList.toggle("toggleError");
+    labelError.classList.add("toggleError");
   } else {
+    labelError.classList.remove("toggleError");
+
+    main.classList.remove("display");
+    main.classList.add("noDisplay");
+
+    popUpDiv.classList.remove("noDisplay");
+    popUpDiv.classList.add("display");
+
+    popupDetail.innerHTML = ` A confirmation email has been sent to
+    <span class="bold">${inputEmail.value}</span>. Please open it and click the
+    button inside to confirm your subscription.`;
+
     console.log("success");
-    labelError.classList.toggle("toggleError");
   }
+});
+
+popUpBtn.addEventListener("click", () => {
+  popUpDiv.classList.remove("display");
+  popUpDiv.classList.add("noDisplay");
+  main.classList.remove("noDisplay");
+  main.classList.add("display");
+  inputEmail.value = "";
 });
 
 const validateEmail = (email) => {
